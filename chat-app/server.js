ec31d663bcbb36a414be3bd5020387f39848af22
Nihat <senius.33@gmail.com>
2021-07-23 22:44:35 +0300

    Update server.js

--- chat-app/server.js
+++ chat-app/server.js
@@ -26,20 +26,3 @@ io.on("connection", function (socket) {
   });
 });
 
-// io.on("connection", (socket) => {
-//   console.log(socket.id);
-
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//     console.log("User Joined Room: " + data);
-//   });
-
-//   socket.on("send_message", (data) => {
-//     console.log(data);
-//     socket.to(data.room).emit("receive_message", data.content);
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("USER DISCONNECTED");
-//   });
-// });
